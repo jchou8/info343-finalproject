b8973ca057809b5eb4164ec8e9f1ad507a3a43c9
Joseph Chou <joseph98c@gmail.com>
2017-12-04 12:01:48 -0800

    add individual files for components

---
+++ src/components/Link.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Link extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
---
+++ src/components/SignUpPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class SignUpPage extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
