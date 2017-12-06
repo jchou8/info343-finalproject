a0f4bbf8bc98d90b526e923b106fa8272fdfb263
michaelessaf <michaelessaf@gmail.com>
2017-12-06 11:21:02 -0800

    implemented linklist.js, will render list of bookmark objects once they are implemented

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
+import Bookmark from "./Bookmark.js";
+import firebase from 'firebase/app';
 
 export default class LinkList extends Component {
+  //takes in one prop folderName and renders list of Bookmark objects 
+  constructor(props){
+    super(props);
+    this.state = {
+                  BookmarkList:[],
+                  shared: false
+                 };
+    this.state.BookmarkList = firebase.database().ref('folders/'+ this.props.folderName);
+  }
   render() {
     if (!this.props.user) {
       // Redirect the user if they're not logged in
       return <Redirect to='/login' />;
     } else {
+      let content = [];
+        for (var i = 0; i < this.state.BookmarkList.length; i++) {
+          <li>
+            content.push(<Bookmark bookmark={this.state.bookmarkList[i]} />);
+            {/*passes in one prop that is a Bookmark object*/}
+          </li>
+      }
+
       return (
         <div>
+          <ol>
+          {content}
+          </ol>
         </div>
       );
     }
