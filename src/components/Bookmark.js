ab555d61eb9d75e48a59c2c74d008f5808f8c879
michaelessaf <michaelessaf@gmail.com>
2017-12-06 12:14:26 -0800

    commit before pulling

--- src/components/Bookmark.js
+++ src/components/Bookmark.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import firebase from 'firebase/app';
+import { Container, Row, Col } from 'reactstrap';
 
 export default class Bookmark extends Component {
+  //takes in BookMarkList prop
+  constructor(props){
+    super(props);
+    this.state = {
+      url:'',
+      name:'',
+      time:'',
+    };
+  }
+
   render() {
+    let date = this.props.date;
+    date = new Date( date );
     return (
       <div>
+      <Row>
+         <Col>this.props.name</Col>
+         <Col><a href={this.props.URL}>{this.props.URL}</a></Col>
+         <Col>{date}</Col>
+      </Row>
+      <i class="fa fa-trash-o" onClick={this.props.handleDelete}></i>
       </div>
     );
   }
--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -4,44 +4,41 @@ import Bookmark from "./Bookmark.js";
 import firebase from 'firebase/app';
 
 export default class LinkList extends Component {
-<<<<<<< HEAD
   //takes in one prop folderName and renders list of Bookmark objects
   constructor(props){
-=======
-  //takes in one prop folderName and renders list of Bookmark objects 
-  constructor(props) {
->>>>>>> 7590e09d973205b84b84103a9d744e5f5e84caff
     super(props);
     this.state = {
       BookmarkList: [],
       shared: false
     };
-    this.state.BookmarkList = firebase.database().ref('folders/' + this.props.folderName);
+    this.state.BookmarkList = firebase.database().ref('folders/' + this.props.folderName +"/links");
   }
-  
+
+
+  handleDelete(bookmarkURL){
+    let arr = this.BookmarkList;
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i].URL == bookmarkURL){
+        arr.splice(i, 1);
+        break;
+      }
+    }
+    this.setState({BookmarkList: arr})
+  }
+
+
   render() {
-<<<<<<< HEAD
     if (!this.props.user) {
       // Redirect the user if they're not logged in
       return <Redirect to='/login' />;
     } else {
       let content = [];
         for (var i = 0; i < this.state.BookmarkList.length; i++) {
-          
-            content.push(<li><Bookmark bookmark={this.state.bookmarkList[i]} /></li>);
+
+            content.push(<li><Bookmark bookmark={this.state.bookmarkList[i]} handleDelete={this.handleDelete.bind(this)}/></li>);
             {/*passes in one prop that is a Bookmark object*/}
 
-      }
-=======
-    let content = [];
-    for (var i = 0; i < this.state.BookmarkList.length; i++) {
-      <li>
-        content.push(<Bookmark bookmark={this.state.bookmarkList[i]} />);
-            {/*passes in one prop that is a Bookmark object*/}
-      </li>
     }
->>>>>>> 7590e09d973205b84b84103a9d744e5f5e84caff
-
     return (
       <div>
         <ol>
@@ -50,4 +47,5 @@ export default class LinkList extends Component {
       </div>
     );
   }
+}
 }
