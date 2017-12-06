7590e09d973205b84b84103a9d744e5f5e84caff
Joseph Chou <joseph98c@gmail.com>
2017-12-06 11:40:04 -0800

    fix autoredirect

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -13,26 +13,22 @@ export default class LinkList extends Component {
     };
     this.state.BookmarkList = firebase.database().ref('folders/' + this.props.folderName);
   }
+  
   render() {
-    if (!this.props.user) {
-      // Redirect the user if they're not logged in
-      return <Redirect to='/login' />;
-    } else {
-      let content = [];
-      for (var i = 0; i < this.state.BookmarkList.length; i++) {
-        <li>
-          content.push(<Bookmark bookmark={this.state.bookmarkList[i]} />);
+    let content = [];
+    for (var i = 0; i < this.state.BookmarkList.length; i++) {
+      <li>
+        content.push(<Bookmark bookmark={this.state.bookmarkList[i]} />);
             {/*passes in one prop that is a Bookmark object*/}
-        </li>
-      }
-
-      return (
-        <div>
-          <ol>
-            {content}
-          </ol>
-        </div>
-      );
+      </li>
     }
+
+    return (
+      <div>
+        <ol>
+          {content}
+        </ol>
+      </div>
+    );
   }
 }
