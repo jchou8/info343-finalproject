504314af0939881b10f6e7c2dc6995b7f1e4c8e0
Joseph Chou <joseph98c@gmail.com>
2017-12-07 00:14:40 -0800

    minor tweaks to bookmark renderer so it doesn't crash

--- src/components/Bookmark.js
+++ src/components/Bookmark.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import firebase from 'firebase/app';
 import { Container, Row, Col } from 'reactstrap';
+import Time from 'react-time';
 
 export default class Bookmark extends Component {
   //takes in BookMarkList prop
@@ -10,14 +11,13 @@ export default class Bookmark extends Component {
 
   render() {
     let date = this.props.bookmark.Date;
-    date = new Date( date );
     //todo: needs a handleDelete() callback to linkedlist on the FA icon
     return (
       <div>
       <Row>
-         <Col>this.props.bookmark.Name</Col>
+         <Col>{this.props.bookmark.Name}</Col>
          <Col><a href={this.props.bookmark.URL}>{this.props.bookmark.URL}</a></Col>
-         <Col>{date}</Col>
+         <Col><Time value={date} relative /></Col>
       </Row>
         <i class="fa fa-trash-o"></i>
       </div>
